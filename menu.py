f90b3fbc2634aa69382c25e033c7e9b6cb3a794b
AlexBondage <gak122007@gmail.com>
2023-06-01 15:48:52 +0300

    Starting TEST

---
+++ menu.py
@@ -0,0 +1,28 @@
+import pygame_menu
+from pygame_menu.examples import create_example_window
+
+
+surface = create_example_window('Snake-menu', (800, 600))
+
+
+
+def start_the_game():
+    print(f'{user_name.get_value()}, - имя')
+    print(f'{user_pass.get_value()}, - пароль')
+    import main
+
+menu = pygame_menu.Menu(
+    height=600,
+    theme=pygame_menu.themes.THEME_GREEN,
+
+    title='Snake',
+    width=800
+)
+
+user_name = menu.add.text_input('Имя: ', default='User', maxchar=10)
+user_pass = menu.add.text_input('Пароль: ', default='123', maxchar=10)
+menu.add.button('Играть', start_the_game)
+menu.add.button('Выйти', pygame_menu.events.EXIT)
+
+if __name__ == '__main__':
+    menu.mainloop(surface)
